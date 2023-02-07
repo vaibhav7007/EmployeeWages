34add4be0a7ac9d858f1ab47f9d3f7188e482343
vaibhav <vaibhavmehta524@gmail.com>
2023-02-08 03:03:15 +0530

    Calculate daily employee wage

---
+++ EmployeeWages/EmployeeWagesUC2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class EmployeeWagesUC2
+    {
+        public static void EmployeeUC2(int empCheck1)
+        {
+            int IS_PRESENT = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWages = 0;
+
+            if (empCheck1 == IS_PRESENT)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWages = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wages: " + empWages);
+        }
+    }
+}
--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -7,7 +7,7 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter option given below");
-            Console.WriteLine("1:UC1 Employee is present or absent");
+            Console.WriteLine("1:UC1 Employee is present or absent\n2:UC2 Employee Wage");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -15,6 +15,12 @@ namespace EmployeeWages
                     Random random = new Random();
                     int empCheck = random.Next(0, 2);
                     EmployeeWagesUC1.EmployeeUC1(empCheck);
+                    break;
+
+                case 2:
+                    Random random1 = new Random();
+                    int empCheck1 = random1.Next(0, 2);
+                    EmployeeWagesUC2.EmployeeUC2(empCheck1);
                     break;
                 }
             }
