793770a83a13f3fec7e0d37da656cc1c0a18108d
vaibhav <vaibhavmehta524@gmail.com>
2023-02-08 03:10:36 +0530

    Add part time employee & wage

---
+++ EmployeeWages/EmployeeWagesUC3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class EmployeeWagesUC3
+    {
+        public static void EmployeeUC3(int empCheck2)
+        {
+            //Initialize input
+            int IS_FULLTIME = 1;
+            int IS_PARTTIME = 2;
+            int EMP_RATE_PER_HRS = 20;
+            int empHrs = 0;
+            int empWages = 0;
+
+            //if statement to check condition
+            if (empCheck2 == IS_FULLTIME)
+            {
+                empHrs = 8;
+                Console.WriteLine("Employee is present and doing Fulltime");
+            }
+            else if (empCheck2 == IS_PARTTIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is present and doing Parttime");
+            }
+            else
+            {
+                empHrs = 0;
+                Console.WriteLine("Employee is absent");
+            }
+            empWages = empHrs * EMP_RATE_PER_HRS;
+            Console.WriteLine("Employee wages:" + empWages);
+
+        }
+    }
+}
--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -7,7 +7,7 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter option given below");
-            Console.WriteLine("1:UC1 Employee is present or absent\n2:UC2 Employee Wage");
+            Console.WriteLine("1:UC1 Employee is present or absent\n2:UC2 Employee Wage\n3:UC3 Employee Wgaes");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -21,6 +21,12 @@ namespace EmployeeWages
                     Random random1 = new Random();
                     int empCheck1 = random1.Next(0, 2);
                     EmployeeWagesUC2.EmployeeUC2(empCheck1);
+                    break;
+
+                case 3:
+                    Random random2 = new Random();
+                    int empCheck2 = random2.Next(0, 3);
+                    EmployeeWagesUC3.EmployeeUC3(empCheck2);
                     break;
                 }
             }
