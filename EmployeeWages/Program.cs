b2e5c9728af944a80bc1aa6226753b08c8e37261
vaibhav <vaibhavmehta524@gmail.com>
2023-02-08 02:52:41 +0530

    Check employee present or absent

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -6,10 +6,17 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wages Prgrams");
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            EmployeeWagesUC1.EmployeeUC1(empCheck);
-        }
+            Console.WriteLine("Please enter option given below");
+            Console.WriteLine("1:UC1 Employee is present or absent");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Random random = new Random();
+                    int empCheck = random.Next(0, 2);
+                    EmployeeWagesUC1.EmployeeUC1(empCheck);
+                    break;
+                }
+            }
     }
 }
